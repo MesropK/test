b430e315a59c4380c69c60c395a9f63e06da7310
Mesrop Kareyan <Mesrop@Mesbook.local>
2017-02-04 20:24:14 +0400

    changed test added testmenst h

--- testmest.cpp
+++ testmest.cpp
@@ -1,10 +1,9 @@
 //
-//  test.m
+//  testmest.cpp
 //  test
 //
 //  Created by Mesrop Kareyan on 2/4/17.
 //  Copyright © 2017 Mesrop Kareyan. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
-#import "testmest.hpp"
+#include "testmest.hpp"
