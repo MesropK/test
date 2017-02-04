7aec0e82d440efbe2620fb0186e9974bc293ce71
Mesrop Kareyan <Mesrop@Mesbook.local>
2017-02-04 20:21:37 +0400

    added testmest

---
+++ testmest.hpp
@@ -0,0 +1,14 @@
+//
+//  testmest.hpp
+//  test
+//
+//  Created by Mesrop Kareyan on 2/4/17.
+//  Copyright © 2017 Mesrop Kareyan. All rights reserved.
+//
+
+#ifndef testmest_hpp
+#define testmest_hpp
+
+#include <stdio.h>
+
+#endif /* testmest_hpp */
